e6aefc0a1af7eeb6384bddcf9dde8687b50ac074
RohitArjun19 <160697309+RohitArjun19@users.noreply.github.com>
2024-09-23 18:21:32 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+document.getElementById('user1').addEventListener('click', function() {
+    window.location.href = 'service.html';
+  });
+  
+  document.getElementById('user2').addEventListener('click', function() {
+     window.location.href = 'profile.html';
+  });
+  
